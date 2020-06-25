cc589106efd5f641fd50b312394992fe0eabe672
Tom Devlin <tomdevlin@Toms-MacBook-Pro.local>
2020-06-25 10:49:10 +0100

    Initial Commit

---
+++ resources/js/new-tab-script.js
@@ -0,0 +1,108 @@
+function startTime() {
+	var today = new Date();
+  	var h = today.getHours();
+  	var m = today.getMinutes();
+	var s = today.getSeconds();
+	m = checkTime(m);
+	s = checkTime(s);
+	document.getElementById('time').innerHTML =
+	h + "-" + m + "-" + s;
+	var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+	if (i < 10) {i = "0" + i};  
+	return i;
+}
+
+function getDay(){
+	var day;
+	switch (new Date().getDay()) {
+	  case 0:
+		   day = "Sunday";
+		    break;
+	  case 1:
+	    day = "Monday";
+	    break;
+	  case 2:
+	     day = "Tuesday";
+	    break;
+	  case 3:
+	    day = "Wednesday";
+	    break;
+	  case 4:
+	    day = "Thursday";
+	    break;
+	  case 5:
+	    day = "Friday";
+	    break;
+	  case 6:
+	    day = "Saturday";
+	}
+	document.getElementById("day").innerHTML = day;
+}
+
+function getDate(){
+	var now = new Date();
+
+	var date = now.getDate();
+
+	var month;
+	switch(now.getMonth()){
+		case 0:
+			month = "Jan"
+			break;
+		case 1:
+			month = "Feb"
+			break;
+		case 2:
+			month = "Mar"
+			break;
+		case 3:
+			month = "Apr"
+			break;
+		case 4:
+			month = "May"
+			break;
+		case 5:
+			month = "Jun"
+			break;
+		case 6:
+			month = "Jul"
+			break;
+		case 7:
+			month = "Aug"
+			break;
+		case 8:
+			month = "Sep"
+			break;
+		case 9:
+			month = "Oct"
+			break;
+		case 10:
+			month = "Nov"
+			break;
+		case 11:
+			month = "Dec"
+			break;
+	}
+
+	var year = now.getFullYear();
+
+	document.getElementById("date").innerHTML = date;
+	document.getElementById("month").innerHTML = month;
+	document.getElementById("year").innerHTML = year;
+
+
+}
+
+function loadQuote(){
+
+}
+
+$(document).ready(function(){
+	startTime();
+	getDay();
+	getDate();
+	loadQuote();
+});
