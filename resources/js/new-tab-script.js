43dbf6d8a3c78af210cbe3fe14c5d957b2e36da9
Tom Devlin <tomdevlin@Toms-MacBook-Pro.local>
2020-07-10 15:04:15 +0100

    commit message

--- resources/js/new-tab-script.js
+++ resources/js/new-tab-script.js
@@ -1,3 +1,5 @@
+var list;
+
 function startTime() {
 	var today = new Date();
   	var h = today.getHours();
@@ -92,17 +94,56 @@ function getDate(){
 	document.getElementById("date").innerHTML = date;
 	document.getElementById("month").innerHTML = month;
 	document.getElementById("year").innerHTML = year;
-
-
 }
 
 function loadQuote(){
-
+	// var quoteArray = [];
+	// $.getJSON("./resources/js/quotes.json"), function(data){
+	// 	quoteArray = JSON.parse(data.quotes);
+	// 	console.log(quoteArray);
+	// };
 }
+
+// function getLocation(){
+// 	if(navigator.geolocation){
+// 		navigator.geolocation.getCurrentPosition(showPosition);
+// 	}
+// 	else{
+// 		alert('problem getting location');
+// 	}
+// }
+
+// function showPosition(position){
+// 	$.ajax({
+// 		type: 'GET',
+// 		url: "https://geocode.xyz/"+position.coords.latitude+","+position.coords.longitude+"?geoit=JSON"
+// 		//dataType: 'JSON',
+// 		success: renderInfo(data)
+// 	});
+// }
+
+// function renderInfo(data){
+// 	console.log(data);
+// }
+
+// function getWeather(){
+// 	$.ajax({
+// 		url: weatherURL,
+// 		type: 'GET',
+// 		dataType: 'json',
+// 		success: function(data){
+// 			console.log(gotJSON)
+// 			list = data == null ? [] : (data instanceof Array ? data : [data]);
+// 			console.log(list);
+// 		}
+// 	});
+// }
+
 
 $(document).ready(function(){
 	startTime();
 	getDay();
 	getDate();
 	loadQuote();
+
 });
